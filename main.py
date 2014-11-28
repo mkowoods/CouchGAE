cb5194deadb8ac110c99e48c0f0acfadba7a4218
M Woods <mkowoods@gmail.com>
2014-11-27 22:38:16 -0800

    made some more cool updates

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,37 +15,42 @@
 # limitations under the License.
 #
 import os
+import time
 import webapp2
 import jinja2
 import logging
-#import time
+import json
+import time
 from google.appengine.ext import db
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 TEMPLATES = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
-                                extensions=['jinja2.ext.autoescape'],
-                                autoescape=True)
+                               extensions=['jinja2.ext.autoescape'],
+                               autoescape=True)
 
 
-
-#Errors
-class dbNotFoundException(Exception):
+# Errors
+class DBNotFoundException(Exception):
     """Exception where db is not found in the Data Store"""
-    api_response = '{"status": "error", \
-                    "message": "Database Does Not Exist"}'
+    api_response = {"status": "error",
+                    "message": "Database Does Not Exist"}
+
 
 class CreateWithoutValException(Exception):
+    api_response = {"status": "error",
+                    "message": "Attempted To Create Record without Value"}
 
-    api_response = '{"status": "error", \
-                    "message": "Attempted To Create Record without Value"}'
 
 class MissingEssentialFieldException(Exception):
+    api_response = {"status": "error",
+                    "message": "Missing a Critical Field (action, db, key...)"}
 
-    api_response = '{"status": "error", \
-                    "message": "Missing a Critical Field (action, db, key...)"}'
 
 class KeyAlreadyExistsError(Exception):
-    pass
+    api_response = {"status": "error",
+                    }
+
+
 
 class KeyNotFoundError(Exception):
     pass
@@ -57,20 +62,34 @@ class KeyValueStore(db.Model):
     The rec_class serves the function of a table from a standard SQL Database. A way of organizing like records together.
     The Key is designed to be unique to facilitate quick look ups.
     """
-    create_date = db.DateTimeProperty(auto_now_add = True)
-    rec_class = db.StringProperty(required = True)
-    store_key = db.StringProperty(required = True)
-    store_val = db.StringProperty(required = True)
+
+    create_date = db.DateTimeProperty(auto_now_add=True)
+    epoch_time = db.StringProperty()
+    rec_class = db.StringProperty(required=True)
+    store_key = db.StringProperty(required=True)
+    store_val = db.StringProperty(required=True)
+
+
+    def json_response(self):
+        response = {
+            'status': 'OK',
+            'created_date': str(self.create_date),
+            'rec_class': self.rec_class,
+            'key': self.store_key,
+            'epoch_time': self.epoch_time,
+            'value': self.store_val
+        }
+        return response
 
 
 class DBStore1(KeyValueStore):
     """First Key Value Store"""
     pass
 
-class DevStore(KeyValueStore):
-    "Key Value Store for "
-    pass
 
+class DevStore(KeyValueStore):
+    """Key Value Store for """
+    pass
 
 
 #Controller
@@ -79,8 +98,7 @@ DB_TO_MODEL_MAP = {'dbstore1': DBStore1,
 
 
 class APIController():
-
-    def __init__(self, action, db, rec_class = "", key = "", val = "", offset = ""):
+    def __init__(self, action, db, rec_class="", key="", val="", offset=""):
         self.key = key
         self.rec_class = rec_class
         self.action = action
@@ -89,14 +107,13 @@ class APIController():
         #TODO: Figure out a better way to handle this exception
         try:
             self.offset = int(offset)
-        except:
+        except ValueError:
             self.offset = 0
 
         if db in DB_TO_MODEL_MAP:
             self.db = DB_TO_MODEL_MAP[db]
         else:
-            raise dbNotFoundException()
-
+            raise DBNotFoundException()
 
     def create(self):
         if self.val == "":
@@ -106,24 +123,31 @@ class APIController():
 
         if self.db.get_by_key_name(self.key):
             raise KeyAlreadyExistsError
-        rec = self.db(key_name = self.key, rec_class = self.rec_class, store_key = self.key, store_val = self.val)
+
+        time_stamp = str(int(time.time()*10000))
+
+        rec = self.db(key_name=self.key,
+                      epoch_time = time_stamp,
+                      rec_class=self.rec_class,
+                      store_key=self.key,
+                      store_val=self.val)
         put = db.put(rec)
         return put.id_or_name()
 
     def read(self):
-        rec = self.db.get_by_key_name(key_names = self.key)
+        rec = self.db.get_by_key_name(key_names=self.key)
         if not rec:
             raise KeyNotFoundError
-        return rec
+        return rec.json_response()
 
-    def read_rec_class(self, offset = None):
+    def read_rec_class(self, offset=None):
         q = db.Query(self.db)
         q.filter('rec_class =', self.rec_class)
         limit = 25
 
-        results = q.fetch(limit = limit, offset = self.offset)
-        for r in results:
-            logging.info(r.store_key+'|'+r.store_val)
+        results = q.fetch(limit=limit, offset=self.offset)
+
+        return [r.json_response() for r in results]
 
     def update(self):
         pass
@@ -131,18 +155,16 @@ class APIController():
     def delete(self):
         pass
 
-
     def run_action(self):
 
         if self.action == "create":
             return self.create()
 
         elif self.action == "read":
-            rec = self.read()
-            return rec.store_key+'|'+rec.store_val
+            return self.read()
 
         elif self.action == "read_rec_class":
-            self.read_rec_class()
+            return self.read_rec_class()
 
 
 #View
@@ -150,7 +172,6 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = TEMPLATES.get_template('index.html')
         self.response.write(template.render())
-
 
 
 class APIHandler(webapp2.RequestHandler):
@@ -166,26 +187,25 @@ class APIHandler(webapp2.RequestHandler):
         if action == "" or db == "":
             self.response.write(MissingEssentialFieldException.api_response)
 
-        API =  APIController(action, db, rec_class, key, val, offset)
+        API = APIController(action, db, rec_class, key, val, offset)
 
         try:
             api_response = API.run_action()
         except CreateWithoutValException:
             api_response = CreateWithoutValException.api_response
-        except dbNotFoundException:
-            api_response = dbNotFoundException.api_response
+        except DBNotFoundException:
+            api_response = DBNotFoundException.api_response
+        except KeyAlreadyExistsError:
+            api_response = KeyAlreadyExistsError.api_response
 
-        self.response.write(api_response)
-
-
-
-
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(api_response, indent=4))
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/api', APIHandler),
-], debug=True)
+                                  ('/', MainHandler),
+                                  ('/api', APIHandler),
+                              ], debug=True)
 
 
 
